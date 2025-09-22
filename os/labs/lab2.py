399f225c96829d5de405fb3eb4bc6e51b9e709fe
7KZH <125111483+7KZH@users.noreply.github.com>
2025-09-22 15:45:25 +0800

    Update lab2.py

--- os/labs/lab2.py
+++ os/labs/lab2.py
@@ -5,5 +5,4 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.process import Process
 
 def priority_scheduler(procs: list[Process]) -> Process:
-    # 实现优先级调度器：选择 priority 最大的，相同时选更靠前的
     return max(procs, key=lambda p: p.priority)
