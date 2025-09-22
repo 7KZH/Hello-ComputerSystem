60809eee10483e2dfa46c010a33bdf1f945bfd8e
7KZH <125111483+7KZH@users.noreply.github.com>
2025-09-22 15:45:44 +0800

    Update lab1.py

--- os/labs/lab1.py
+++ os/labs/lab1.py
@@ -5,6 +5,4 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.process import Process
 
 def sequential_scheduler(procs: list[Process]) -> Process:
-    # TODO: implement a sequential scheduler
-    # Simple implementation: return the first process
     return procs[0]
